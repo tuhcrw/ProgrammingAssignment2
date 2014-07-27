0bd343ae9d7df9439ce09e444cdf040ffcb1fb9a
Chris Wilson <chriswilson1974@gmail.com>
2014-07-27 21:32:03 +1000

    With comments, cleaned the code up a bit and a better understanding

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Creates a function that takes in a matrix. The makeCacheMatrix has a get & set function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## Still not really sure what this does I think the best explanation I found is in the forum
+  ## https://class.coursera.org/rprog-005/forum/thread?thread_id=967 
   m<-NULL
   set<-function(y){
     x<<-y
     m<<-NULL
   }
-  get<-function() x
-  setmatrix<-function(solve) m<<- solve
-  getmatrix<-function() m
+  ##The commands to obtain the calculations on the inputted matrix
+  get<-function() x  ##Obtains the data inputted
+  setmatrix<-function(solve) m<<- solve  ##calculates the inverse and adds it to the variable 
+  getmatrix<-function() m  ##Returns the inverse of m when asked to from another function
   list(set=set, get=get,
        setmatrix=setmatrix,
        getmatrix=getmatrix)
@@ -20,15 +20,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x=matrix(), ...) {
   m<-x$getmatrix()
-  if(!is.null(m)){
+  ##If m is not null return what we have already calculated
+  if(!is.null(m)){ 
     message("getting cached data")
     return(m)
-  }
-  matrix<-x$get()
-  m<-solve(matrix, ...)
-  x$setmatrix(m)
-  m
+  } 
+  else{
+    ## if m is null
+    matrix<-x$get() ##Obtain what was inputted
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    return(m)
+    }
 }
 
-a <- makeCacheMatrix(c(1,2,3,4))
-a$getmatrix()
+
